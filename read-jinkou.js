c3d2e73cae68f0c2acb8c7835f7c9113892a7d4e
takewell <5315053@ed.tus.ac.jp>
2017-07-28 02:14:18 +0900

    init

---
+++ read-jinkou.js
@@ -0,0 +1,31 @@
+'use strict';
+const fs = require('fs');
+const filename = './jinkou.txt';
+
+Promise.resolve({}).then((object) => {
+  return new Promise((re solve) => {
+    let isExist = false;
+    try {
+      fs.startSync(filename);
+      return true;
+    } catch (err) {
+      console.log("Can not read this file")
+      return false;
+    }
+    return resolve(isExist);
+  });
+}).then((filename) => {
+  return new Promise((resolve) => {
+    fs.readFile(filename, { encoding: 'utf-8' }, (err, content) => {
+      return resolve(content);
+    });
+  });
+}).then((content) => {
+  return new Promise((resolve) => {
+    const popu = content.replace(/\d{6,8}/, "");
+    console.log(popu);
+    fs.writeFile(filename + '.txt', popu, () => {
+      return resolev();
+    });
+  });
+});
